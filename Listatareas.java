8e171e3f1a170673619d874daccb9f7bebf82094
ThePabkiXD <143724076+ThePabkiXD@users.noreply.github.com>
2023-09-05 08:43:42 -0600

    Add files via upload

---
+++ Listatareas.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author labcca
+ */
+public class Listatareas {
+    
+    public static void main(String[] args) {
+        String tarea1 = "Hacer expocision";
+        String tarea2 = "Hacer resumen";
+        String tarea3 = "Serie de ejercicios";
+        String tarea4 = "Comprar libro";
+        String tarea5 = "no se";
+        
+        System.out.println("La tarea 1 es: " + tarea1);
+        System.out.println("La tarea 2 es: " + tarea2);
+        System.out.println("La tarea 3 es: " + tarea3);
+        System.out.println("La tarea 4 es: " + tarea4);
+        System.out.println("La tarea 5 es: " + tarea5);
+
+    }
+    
+}
