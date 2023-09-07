fc1f3df3a1943ec8c7abd026f67067473e9c6c7a
ThePabkiXD <143724076+ThePabkiXD@users.noreply.github.com>
2023-09-07 08:44:41 -0600

    Add files via upload

---
+++ Practica2AreaRectangulo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica2 {
+    public static void main(String[] args) {
+        
+    int base = 20;
+    int altura = 10;
+
+    int areaReo = (base * altura);
+
+System.out.println("Mi nombre completo es Juan Pablo Rios Trujillo");
+System.out.println("El area del rectangulo es: " + areaReo);
+
+
+    }
+    
+}
